c2d2a4b495a085bdf6c95e241695eb68d576b3ba
Hugo <hugomaranse@gmail.com>
2025-01-31 19:40:34 -0300

    Código finalizado e pronto para uso

--- botao_e_leds_apagando.c
+++ botao_e_leds_apagando.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/time.h"
 
 #define botao 5
 #define red_led 11
 #define yellow_led 12
 #define green_led 13
 
+static bool leds_ativos = false;
+uint16_t delay; 
 uint8_t i;
-struct repeating_timer timer;
 static volatile uint32_t last_time = 0;
+
+int64_t turn_off_callback(alarm_id_t id, void* user_data) {
+    uint led = (uint)(uintptr_t)user_data; 
+    gpio_put(led, 0);  
+    leds_ativos = (gpio_get(13) != 0);
+    return 0; 
+}
 
 void botinit(){
     gpio_init(5);
@@ -25,25 +34,23 @@ void ledinit(){
 }
 
 static void gpio_irq_handler(uint gpio, uint32_t events){
-    uint32_t current_time = to_us_since_boot(get_absolute_time());
-    if(current_time - last_time > 200000){
-        last_time = current_time;
-            for(i = 11; i < 14 ; i++){
-            gpio_put(i, 1);
-            }
-    }    
-
+    if(!leds_ativos){ 
+        uint32_t current_time = to_us_since_boot(get_absolute_time());
+        if(current_time - last_time > 200000){
+            last_time = current_time;
+                for(i = 11, delay = 3000; i < 14 ; i++, delay += 3000){
+                gpio_put(i, 1);
+                add_alarm_in_ms(delay, turn_off_callback, (void*)(uintptr_t)i, false);
+                }
+    }    }      
 }
 
 int main()
 {   
-    stdio_init_all();
     botinit();
     ledinit();
     gpio_set_irq_enabled_with_callback(5, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        sleep_ms(1);
     }
-}
+} 
